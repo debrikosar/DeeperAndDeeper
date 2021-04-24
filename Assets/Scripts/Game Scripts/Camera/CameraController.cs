ec3445a8a806518356d2fd96869c92134c9c21d1
AndreiSnihur <ronnim101@yandex.ru>
2021-04-24 19:37:33 +0300

    Restricted camera movement

--- Assets/Scripts/Game Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Game Scripts/Camera/CameraController.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    [SerializeField] private Transform player;
+    [SerializeField] private SpriteRenderer sea;
+    private float restrictX;
+    private float restrictYup;
+    private float restrictYdown;
+
+    private void Start()
+    {
+        float cameraSize = Camera.main.orthographicSize;
+        restrictX = sea.size.x/2 - cameraSize*1.75f;
+        restrictYdown = sea.size.y / 2 - cameraSize*0.95f;
+        restrictYup = sea.size.y/ 2;
+        print("X" + restrictX);
+        print("Yup" + restrictYup);
+        print("Ydown" + restrictYdown);
+    }
 
     // Update is called once per frame
-    void LateUpdate()
+    private void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, -restrictX, restrictX),
+                                         Mathf.Clamp(player.position.y, -restrictYdown, restrictYup),
+                                         transform.position.z);
     }
 }
